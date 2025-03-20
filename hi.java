731aa219e4c5e86cdfcec5ce421062ae261b5d22
Nisarga2304 <nisarga23024@gmail.com>
2025-03-20 12:48:52 +0530

    Create hi.java

---
+++ hi.java
@@ -0,0 +1,13 @@
+public class hi {
+    public static void main(String[] args) {
+        // Hardcoded numbers
+        double num1 = 10.5;
+        double num2 = 20.3;
+
+        // Add the numbers
+        double sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
